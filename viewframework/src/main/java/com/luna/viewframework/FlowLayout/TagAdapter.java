2756926f3b0ee8732df17d1cce54b3e5ae0468df
胡芳林 <hufanglin@hemaapp.com>
2017-12-07 14:44:01 +0800

    添加AnimateCheckBox

---
+++ viewframework/src/main/java/com/luna/viewframework/FlowLayout/TagAdapter.java
@@ -0,0 +1,82 @@
+package com.luna.viewframework.FlowLayout;
+
+
+
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Hufanglin on 2016/2/23.
+ */
+public abstract class TagAdapter<T>
+{
+    protected List<T> mTagDatas;
+    private OnDataChangedListener mOnDataChangedListener;
+    private HashSet<Integer> mCheckedPosList = new HashSet<Integer>();
+
+    public TagAdapter(List<T> datas)
+    {
+        mTagDatas = datas;
+    }
+
+    public TagAdapter(T[] datas)
+    {
+        mTagDatas = new ArrayList<T>(Arrays.asList(datas));
+    }
+
+    static interface OnDataChangedListener
+    {
+        void onChanged();
+    }
+
+    void setOnDataChangedListener(OnDataChangedListener listener)
+    {
+        mOnDataChangedListener = listener;
+    }
+
+    public void setSelectedList(int... pos)
+    {
+        for (int i = 0; i < pos.length; i++)
+            mCheckedPosList.add(pos[i]);
+        notifyDataChanged();
+    }
+    public void setSelectedList(Set<Integer> set)
+    {
+        mCheckedPosList.clear();
+        mCheckedPosList.addAll(set);
+        notifyDataChanged();
+    }
+
+    HashSet<Integer> getPreCheckedList()
+    {
+        return mCheckedPosList;
+    }
+
+
+    public int getCount()
+    {
+        return mTagDatas == null ? 0 : mTagDatas.size();
+    }
+
+    public void notifyDataChanged()
+    {
+        mOnDataChangedListener.onChanged();
+    }
+
+    public T getItem(int position)
+    {
+        return mTagDatas.get(position);
+    }
+
+    public List<T> getTagDatas() {
+        return mTagDatas;
+    }
+
+    public abstract View getView(FlowLayout parent, int position, T t);
+
+}
